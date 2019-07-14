6a3301ac8f96bdb1184929208598dbf91db8f895
ghostbuster91 <kghost0@gmail.com>
2019-07-14 09:37:58 +0200

    Remove unnecessary DiffFor[BigDecimal] && DiffFor[UUID]

--- core/src/main/scala/asserts/diff/DiffForInstances.scala
+++ core/src/main/scala/asserts/diff/DiffForInstances.scala
@@ -1,14 +1,12 @@
 package asserts.diff
 
-import java.util.UUID
-
 trait DiffForInstances extends LowPriorityDiffForInstances {
 
   implicit def diffForOption[T: DiffFor]: DiffFor[Option[T]] = (left: Option[T], right: Option[T]) => {
     (left, right) match {
       case (Some(l), Some(r)) => implicitly[DiffFor[T]].diff(l, r)
-      case (None, None)       => Identical(None)
-      case (l, r)             => DiffResultValue(l, r)
+      case (None, None) => Identical(None)
+      case (l, r) => DiffResultValue(l, r)
     }
   }
 
@@ -22,7 +20,7 @@ trait DiffForInstances extends LowPriorityDiffForInstances {
         index.toString -> (implicitly[DiffFor[Option[T]]].diff(leftAsMap(index), rightAsMap(index)) match {
           case DiffResultValue(Some(v), None) => DiffResultAdditional(v)
           case DiffResultValue(None, Some(v)) => DiffResultMissing(v)
-          case d                              => d
+          case d => d
         })
       }.toMap
     )
@@ -35,7 +33,4 @@ trait DiffForInstances extends LowPriorityDiffForInstances {
         k -> implicitly[DiffFor[Option[T]]].diff(left.get(k), right.get(k))
       }.toMap)
     }
-
-  implicit val diffForBigDecimal: DiffFor[BigDecimal] = DiffFor.apply(_.toString)
-  implicit val diffForUUID: DiffFor[UUID] = DiffFor.apply(_.toString)
 }
