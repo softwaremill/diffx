b07a4408b0579ec3607c3e5a571a0b2d00bf92c7
ghostbuster91 <kghost0@gmail.com>
2019-09-10 09:21:19 +0200

    Remove path access

--- core/src/main/scala/com/softwaremill/diffx/IgnoreMacro.scala
+++ core/src/main/scala/com/softwaremill/diffx/IgnoreMacro.scala
@@ -10,9 +10,6 @@ object IgnoreMacro {
       c: blackbox.Context
   )(path: c.Expr[T => U]): c.Tree = applyIgnored(c)(ignoredFromPathMacro(c)(path))
 
-  /**
-    * A helper method for modifyPimp_impl and modifyAllPimp_impl.
-    */
   private def applyIgnored[T: c.WeakTypeTag, U: c.WeakTypeTag](c: blackbox.Context)(
       path: c.Expr[List[String]]
   ): c.Tree = {
@@ -20,7 +17,7 @@ object IgnoreMacro {
 
     val wrappedValue = c.macroApplication match {
       case Apply(TypeApply(Select(Apply(_, List(w)), _), _), _) => w
-      case _                                                    => c.abort(c.enclosingPosition, s"Unknown usage of ModifyPimp. Please file a bug.")
+      case _                                                    => c.abort(c.enclosingPosition, s"Unknown usage of IgnoreMacroExt. Please file a bug.")
     }
 
     val valueAlias = TermName(c.freshName())
@@ -32,54 +29,16 @@ object IgnoreMacro {
   }
 
   /**
-    * Produce a modification for a single path.
+    * Converts path to list of strings
     */
   def ignoredFromPathMacro[T: c.WeakTypeTag, U: c.WeakTypeTag](c: blackbox.Context)(
       path: c.Expr[T => U]
   ): c.Expr[List[String]] = {
     import c.universe._
 
-    sealed trait PathAccess
-    case object DirectPathAccess extends PathAccess
-    case class SealedPathAccess(types: Set[Symbol]) extends PathAccess
-
     sealed trait PathElement
-    case class TermPathElement(term: c.TermName, access: PathAccess, xargs: c.Tree*) extends PathElement
-    //    case class SubtypePathElement(subtype: c.Symbol) extends PathElement
+    case class TermPathElement(term: c.TermName, xargs: c.Tree*) extends PathElement
     case class FunctorPathElement(functor: c.Tree, method: c.TermName, xargs: c.Tree*) extends PathElement
-
-    /**
-      * Determine if the `.copy` method should be applied directly
-      * or through a match across all subclasses (for sealed traits).
-      */
-    def determinePathAccess(typeSymbol: Symbol) = {
-      def ifEmpty[A](set: Set[A], empty: => Set[A]) =
-        if (set.isEmpty) empty else set
-
-      def knownDirectSubclasses(sealedSymbol: ClassSymbol) = ifEmpty(
-        sealedSymbol.knownDirectSubclasses,
-        c.abort(
-          c.enclosingPosition,
-          s"""Could not find subclasses of sealed trait $sealedSymbol.
-             |You might need to ensure that it gets compiled before this invocation.
-             |See also: <https://issues.scala-lang.org/browse/SI-7046>.""".stripMargin
-        )
-      )
-
-      def expand(symbol: Symbol): Set[Symbol] =
-        Set(symbol)
-          .filter(_.isClass)
-          .map(_.asClass)
-          .map { s =>
-            s.typeSignature; s
-          } // see <https://issues.scala-lang.org/browse/SI-7755>
-          .filter(_.isSealed)
-          .flatMap(s => knownDirectSubclasses(s))
-          .flatMap(s => ifEmpty(expand(s), Set(s)))
-
-      val subclasses = expand(typeSymbol)
-      if (subclasses.isEmpty) DirectPathAccess else SealedPathAccess(subclasses)
-    }
 
     /**
       * _.a.b.each.c => List(TPE(a), TPE(b), FPE(functor, each/at/eachWhere, xargs), TPE(c))
@@ -92,12 +51,11 @@ object IgnoreMacro {
 
       tree match {
         case q"$parent.$child " =>
-          val access = determinePathAccess(parent.tpe.typeSymbol)
-          collectPathElements(parent, TermPathElement(child, access) :: acc)
+          collectPathElements(parent, TermPathElement(child) :: acc)
         case q"$tpname[..$_]($t)($f) " if typeSupported(tpname) =>
           val newAcc = acc match {
             // replace the term controlled by quicklens
-            case TermPathElement(term, _, xargs @ _*) :: rest => FunctorPathElement(f, term, xargs: _*) :: rest
+            case TermPathElement(term, xargs @ _*) :: rest => FunctorPathElement(f, term, xargs: _*) :: rest
             case pathEl :: _ =>
               c.abort(c.enclosingPosition, s"Invalid use of path element $pathEl. $ShapeInfo, got: ${path.tree}")
           }
@@ -113,7 +71,7 @@ object IgnoreMacro {
     }
 
     c.Expr[List[String]](q"${pathEls.collect {
-      case TermPathElement(c, _) => c.decodedName.toString
+      case TermPathElement(c) => c.decodedName.toString
     }}")
   }
 
