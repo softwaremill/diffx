42de329346a94ead4e26ba2aba9f33d582f97369
ghostbuster91 <kghost0@gmail.com>
2019-09-19 23:39:50 +0200

    Fix failing pr's by decrypt files only if not PR

---
+++ scripts/decrypt_files_if_not_pr.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    openssl aes-256-cbc -K $encrypted_5331bd37a5e5_key -iv $encrypted_5331bd37a5e5_iv -in secrets.tar.enc -out secrets.tar -d
+    tar xvf secrets.tar
+fi
