2ced7fba087181b6ad93a2fa86a671ce734a13e1
ghostbuster91 <kghost0@gmail.com>
2019-10-07 10:03:14 +0200

    Assign derived typeclass to a variable

--- specs2/src/main/scala/com/softwaremill/diffx/scalatest/DiffMatcher.scala
+++ specs2/src/main/scala/com/softwaremill/diffx/scalatest/DiffMatcher.scala
@@ -7,24 +7,27 @@ trait DiffMatcher {
   def matchTo[A: Diff](left: A): DiffForMatcher[A] = DiffForMatcher(left)
 
   case class DiffForMatcher[A: Diff](right: A) extends Matcher[A] {
-    override def apply[S <: A](left: Expectable[S]): MatchResult[S] = result(
-      test = {
-        Diff[A].apply(left.value, right) match {
+    override def apply[S <: A](left: Expectable[S]): MatchResult[S] = {
+      val diff = Diff[A]
+      result(
+        test = {
+          diff.apply(left.value, right) match {
+            case c: DiffResultDifferent =>
+              false
+            case _ =>
+              true
+          }
+        },
+        okMessage = "",
+        koMessage = diff.apply(left.value, right) match {
           case c: DiffResultDifferent =>
-            false
+            c.show
           case _ =>
-            true
-        }
-      },
-      okMessage = "",
-      koMessage = Diff[A].apply(left.value, right) match {
-        case c: DiffResultDifferent =>
-          c.show
-        case _ =>
-          ""
-      },
-      left
-    )
+            ""
+        },
+        left
+      )
+    }
   }
 
 }
