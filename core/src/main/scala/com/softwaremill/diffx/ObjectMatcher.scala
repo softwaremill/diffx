49f32cd8085d0ab2930756723dc1c1946ba9ad94
ghostbuster91 <kghost0@gmail.com>
2021-06-24 00:13:11 +0200

    Add scala-docs for ObjectMatcher

--- core/src/main/scala/com/softwaremill/diffx/ObjectMatcher.scala
+++ core/src/main/scala/com/softwaremill/diffx/ObjectMatcher.scala
@@ -1,8 +1,8 @@
 package com.softwaremill.diffx
 
-/*
-  Used to pair elements within unordered containers like sets
- */
+/** Defines how the elements within collections are paired
+  * @tparam T
+  */
 trait ObjectMatcher[T] {
   def isSameObject(left: T, right: T): Boolean
 }
@@ -10,12 +10,16 @@ trait ObjectMatcher[T] {
 object ObjectMatcher extends LowPriorityObjectMatcher {
   def apply[T: ObjectMatcher]: ObjectMatcher[T] = implicitly[ObjectMatcher[T]]
 
+  /** Given product of type T and its property U, match that products using U's objectMatcher */
   def by[T, U: ObjectMatcher](f: T => U): ObjectMatcher[T] = (left: T, right: T) =>
     ObjectMatcher[U].isSameObject(f(left), f(right))
 
+  /** Given key-value (K,V) pairs match them using V's objectMatcher */
   def byValue[K, V: ObjectMatcher]: ObjectMatcher[(K, V)] = ObjectMatcher.by[(K, V), V](_._2)
+
+  /** Given key-value (K,V) pairs, where V is a type of product and U is a property of V, match them using U's objectMatcher */
   def byValue[K, V, U: ObjectMatcher](f: V => U): ObjectMatcher[(K, V)] =
-    ObjectMatcher.by[(K, V), V](_._2)(ObjectMatcher.by[V, U](f))
+    ObjectMatcher.byValue[K, V](ObjectMatcher.by[V, U](f))
 
   implicit def optionMatcher[T: ObjectMatcher]: ObjectMatcher[Option[T]] = (left: Option[T], right: Option[T]) => {
     (left, right) match {
@@ -23,10 +27,11 @@ object ObjectMatcher extends LowPriorityObjectMatcher {
       case _                  => false
     }
   }
+
+  /** Given key-value (K,V) pairs, match them using K's objectMatcher */
   implicit def byKey[K: ObjectMatcher, V]: ObjectMatcher[(K, V)] = ObjectMatcher.by[(K, V), K](_._1)
 }
 
 trait LowPriorityObjectMatcher {
   implicit def default[T]: ObjectMatcher[T] = (l: T, r: T) => l == r
-
 }
