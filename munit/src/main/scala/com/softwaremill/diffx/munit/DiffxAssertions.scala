34ce1fcf103a3adad5d725109bbd8ec35bd963c2
ghostbuster91 <kghost0@gmail.com>
2021-06-23 21:28:57 +0200

    Fix testFrameworks integrations

--- munit/src/main/scala/com/softwaremill/diffx/munit/DiffxAssertions.scala
+++ munit/src/main/scala/com/softwaremill/diffx/munit/DiffxAssertions.scala
@@ -1,15 +1,14 @@
 package com.softwaremill.diffx.munit
 
-import com.softwaremill.diffx.{ConsoleColorConfig, Diff, DiffResultDifferent}
+import com.softwaremill.diffx.{ConsoleColorConfig, Diff}
 import munit.Assertions._
 import munit.Location
 
 trait DiffxAssertions {
   def assertEqual[T: Diff](t1: T, t2: T)(implicit c: ConsoleColorConfig, loc: Location): Unit = {
     val result = Diff.compare(t1, t2)
-    result match {
-      case different: DiffResultDifferent => fail(different.show())(loc)
-      case _                              => // do nothing
+    if (!result.isIdentical) {
+      fail(result.show())(loc)
     }
   }
 }
