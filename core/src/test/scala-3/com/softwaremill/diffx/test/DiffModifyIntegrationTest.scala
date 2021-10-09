422b53976fda78750daad1abece5b9387b916c82
ghostbuster91 <kghost0@gmail.com>
2021-10-09 22:09:48 +0200

    Migrate DiffModifyIntegrationTest

--- core/src/test/scala-3/com/softwaremill/diffx/test/DiffModifyIntegrationTest.scala
+++ core/src/test/scala-3/com/softwaremill/diffx/test/DiffModifyIntegrationTest.scala
@@ -8,34 +8,34 @@ import com.softwaremill.diffx.generic.auto._
 import java.time.Instant
 import java.util.UUID
 
-class DiffModifyIntegrationTest extends AnyFlatSpec with Matchers {
-//  val instant: Instant = Instant.now()
-//  val p1 = Person("p1", 22, instant)
-//  val p2 = Person("p2", 11, instant)
-//
-//  it should "allow importing and exporting implicits" in {
-//    implicit val d: Diff[Person] = Diff.derived[Person].ignore(_.name)
-//    compare(p1, p2) shouldBe DiffResultObject(
-//      "Person",
-//      Map("name" -> IdenticalValue("<ignored>"), "age" -> DiffResultValue(22, 11), "in" -> IdenticalValue(instant))
-//    )
-//  }
-//
-//  it should "allow importing and exporting implicits using macro on derived instance" in {
-//    implicit val d: Diff[Person] = Diff.derived[Person].ignore(_.name)
-//    compare(p1, p2) shouldBe DiffResultObject(
-//      "Person",
-//      Map("name" -> IdenticalValue("<ignored>"), "age" -> DiffResultValue(22, 11), "in" -> IdenticalValue(instant))
-//    )
-//  }
-//
-//  it should "allow calling ignore multiple times" in {
-//    implicit val d: Diff[Person] = Diff.derived[Person]
-//      .ignore(_.name)
-//      .ignore(_.age)
-//    compare(p1, p2).isIdentical shouldBe true
-//  }
-//
+class DiffModifyIntegrationTest extends AnyFlatSpec with Matchers with ScalaVersionSpecificDiffInstances {
+  val instant: Instant = Instant.now()
+  val p1 = Person("p1", 22, instant)
+  val p2 = Person("p2", 11, instant)
+
+  it should "allow importing and exporting implicits" in {
+    implicit val d: Diff[Person] = Diff.derived[Person].ignore(_.name)
+    compare(p1, p2) shouldBe DiffResultObject(
+      "Person",
+      Map("name" -> IdenticalValue("<ignored>"), "age" -> DiffResultValue(22, 11), "in" -> IdenticalValue(instant))
+    )
+  }
+
+  it should "allow importing and exporting implicits using macro on derived instance" in {
+    implicit val d: Diff[Person] = Diff.derived[Person].ignore(_.name)
+    compare(p1, p2) shouldBe DiffResultObject(
+      "Person",
+      Map("name" -> IdenticalValue("<ignored>"), "age" -> DiffResultValue(22, 11), "in" -> IdenticalValue(instant))
+    )
+  }
+
+  it should "allow calling ignore multiple times" in {
+    implicit val d: Diff[Person] = Diff.derived[Person]
+      .ignore(_.name)
+      .ignore(_.age)
+    compare(p1, p2).isIdentical shouldBe true
+  }
+
 //  it should "compare lists using explicit object matcher comparator" in {
 //    val o1 = Organization(List(p1, p2))
 //    val o2 = Organization(List(p2, p1))
@@ -46,36 +46,36 @@ class DiffModifyIntegrationTest extends AnyFlatSpec with Matchers {
 //      )
 //    compare(o1, o2).isIdentical shouldBe true
 //  }
-//
-//  it should "ignore only on right" in {
-//    case class Wrapper(e: Either[Person, Person])
-//    val e1 = Wrapper(Right(p1))
-//    val e2 = Wrapper(Right(p1.copy(name = p1.name + "_modified")))
-//
-//    implicit val wrapperDiff: Diff[Wrapper] = Diff.derived[Wrapper].ignore(_.e.eachRight.name)
-//
-//    compare(e1, e2).isIdentical shouldBe true
-//
-//    val e3 = Wrapper(Left(p1))
-//    val e4 = Wrapper(Left(p1.copy(name = p1.name + "_modified")))
-//
-//    compare(e3, e4).isIdentical shouldBe false
-//  }
-//
-//  it should "ignore only on left" in {
-//    case class Wrapper(e: Either[Person, Person])
-//    val e1 = Wrapper(Right(p1))
-//    val e2 = Wrapper(Right(p1.copy(name = p1.name + "_modified")))
-//
-//    implicit val wrapperDiff: Diff[Wrapper] = Diff.derived[Wrapper].ignore(_.e.eachLeft.name)
-//
-//    compare(e1, e2).isIdentical shouldBe false
-//    val e3 = Wrapper(Left(p1))
-//    val e4 = Wrapper(Left(p1.copy(name = p1.name + "_modified")))
-//
-//    compare(e3, e4).isIdentical shouldBe true
-//  }
-//
+
+  it should "ignore only on right" in {
+    case class Wrapper(e: Either[Person, Person])
+    val e1 = Wrapper(Right(p1))
+    val e2 = Wrapper(Right(p1.copy(name = p1.name + "_modified")))
+
+    implicit val wrapperDiff: Diff[Wrapper] = Diff.derived[Wrapper].ignore(_.e.eachRight.name)
+
+    compare(e1, e2).isIdentical shouldBe true
+
+    val e3 = Wrapper(Left(p1))
+    val e4 = Wrapper(Left(p1.copy(name = p1.name + "_modified")))
+
+    compare(e3, e4).isIdentical shouldBe false
+  }
+
+  it should "ignore only on left" in {
+    case class Wrapper(e: Either[Person, Person])
+    val e1 = Wrapper(Right(p1))
+    val e2 = Wrapper(Right(p1.copy(name = p1.name + "_modified")))
+
+    implicit val wrapperDiff: Diff[Wrapper] = Diff.derived[Wrapper].ignore(_.e.eachLeft.name)
+
+    compare(e1, e2).isIdentical shouldBe false
+    val e3 = Wrapper(Left(p1))
+    val e4 = Wrapper(Left(p1.copy(name = p1.name + "_modified")))
+
+    compare(e3, e4).isIdentical shouldBe true
+  }
+
 //  it should "match map entries by values" in {
 //    implicit val lookupDiff: Diff[MyLookup] = Diff.derived[MyLookup]
 //      .modify(_.map)
@@ -103,7 +103,7 @@ class DiffModifyIntegrationTest extends AnyFlatSpec with Matchers {
 //      )
 //    )
 //  }
-//
+
 //  it should "use overrided object matcher when comparing set" in {
 //    implicit val lookupDiff: Diff[Startup] = Diff.derived[Startup]
 //      .modify(_.workers)
@@ -135,7 +135,7 @@ class DiffModifyIntegrationTest extends AnyFlatSpec with Matchers {
 //      )
 //    )
 //  }
-//
+
 //  it should "compare lists using object matcher comparator passed explicitly" in {
 //    val o1 = Organization(List(p1, p2))
 //    val o2 = Organization(List(p2, p1))
@@ -144,44 +144,44 @@ class DiffModifyIntegrationTest extends AnyFlatSpec with Matchers {
 //      .useMatcher(ObjectMatcher.list[Person].byValue(_.name))
 //    compare(o1, o2)(d).isIdentical shouldBe true
 //  }
-//
-//  it should "allow overriding how ignored diffs are produced" in {
-//    implicit val conf: DiffConfiguration = DiffConfiguration(makeIgnored =
-//      (original: Diff[Any]) =>
-//        (left: Any, right: Any, context: DiffContext) => {
-//          IdenticalValue(
-//            s"Ignored but was: ${original.apply(left, right, context).show()(ConsoleColorConfig.noColors)}"
-//          )
-//        }
-//    )
-//    implicit val d: Diff[Person] = Diff.derived[Person].ignore(_.name)
-//    compare(p1, p2) shouldBe DiffResultObject(
-//      "Person",
-//      Map(
-//        "name" -> IdenticalValue("Ignored but was: p[1 -> 2]"),
-//        "age" -> DiffResultValue(22, 11),
-//        "in" -> IdenticalValue(instant)
-//      )
-//    )
-//  }
-//
-//  it should "allow overriding how ignored diffs are produced - regular instance" in {
-//    implicit val conf: DiffConfiguration = DiffConfiguration(makeIgnored =
-//      (original: Diff[Any]) =>
-//        (left: Any, right: Any, context: DiffContext) => {
-//          IdenticalValue(
-//            s"Ignored but was: ${original.apply(left, right, context).show()(ConsoleColorConfig.noColors)}"
-//          )
-//        }
-//    )
-//    val d: Diff[Person] = Diff[Person].ignore(_.name)
-//    compare(p1, p2)(d) shouldBe DiffResultObject(
-//      "Person",
-//      Map(
-//        "name" -> IdenticalValue("Ignored but was: p[1 -> 2]"),
-//        "age" -> DiffResultValue(22, 11),
-//        "in" -> IdenticalValue(instant)
-//      )
-//    )
-//  }
+
+  it should "allow overriding how ignored diffs are produced" in {
+    implicit val conf: DiffConfiguration = DiffConfiguration(makeIgnored =
+      (original: Diff[Any]) =>
+        (left: Any, right: Any, context: DiffContext) => {
+          IdenticalValue(
+            s"Ignored but was: ${original.apply(left, right, context).show()(ConsoleColorConfig.noColors)}"
+          )
+        }
+    )
+    implicit val d: Diff[Person] = Diff.derived[Person].ignore(_.name)
+    compare(p1, p2) shouldBe DiffResultObject(
+      "Person",
+      Map(
+        "name" -> IdenticalValue("Ignored but was: p[1 -> 2]"),
+        "age" -> DiffResultValue(22, 11),
+        "in" -> IdenticalValue(instant)
+      )
+    )
+  }
+
+  it should "allow overriding how ignored diffs are produced - regular instance" in {
+    implicit val conf: DiffConfiguration = DiffConfiguration(makeIgnored =
+      (original: Diff[Any]) =>
+        (left: Any, right: Any, context: DiffContext) => {
+          IdenticalValue(
+            s"Ignored but was: ${original.apply(left, right, context).show()(ConsoleColorConfig.noColors)}"
+          )
+        }
+    )
+    val d: Diff[Person] = Diff[Person].ignore(_.name)
+    compare(p1, p2)(d) shouldBe DiffResultObject(
+      "Person",
+      Map(
+        "name" -> IdenticalValue("Ignored but was: p[1 -> 2]"),
+        "age" -> DiffResultValue(22, 11),
+        "in" -> IdenticalValue(instant)
+      )
+    )
+  }
 }
